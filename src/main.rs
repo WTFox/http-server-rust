d9bdf41aeeb5d817d2aa06e882e67d64c1e6b126
A. Fox <anthonyfox1988@gmail.com>
2025-09-25 07:41:25 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-#[allow(unused_imports)]
 use std::net::TcpListener;
+use std::{io::Write, net::TcpStream};
+
+fn handle_stream(mut stream: TcpStream) {
+    println!("accepted new connection");
+    let resp = b"HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(resp).unwrap();
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,9 +14,7 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(stream) => handle_stream(stream),
             Err(e) => {
                 println!("error: {}", e);
             }
